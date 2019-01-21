5103b13b488a24f74d07a73f4c8bf54b89662d96
mobin-zaman <mobin_zaman@hotmail.com>
2019-01-21 20:27:51 +0600

    .

---
+++ Generics.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Generics
+{
+    public static void main(String args[]){
+        ArrayList <Integer> items=new ArrayList<Integer>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items); 
+     }
+
+    private static void printDoubled(ArrayList <Integer> n){
+        for(Object i:n){
+            System.out.println((Integer) i*2);
+        }
+    }
+}
